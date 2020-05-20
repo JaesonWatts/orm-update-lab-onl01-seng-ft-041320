acf0a2f3b3ad8946d006306e9a622344944f8629
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-20 21:20:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,13 +60,8 @@ class Student
   end
   
   def self.new_from_db(row)
-    
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
-     
+    student_new = self.new(row[0], row[1], row[2])
+    student_new
   end
   
    def self.find_by_name(name)
